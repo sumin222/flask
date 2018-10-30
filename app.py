2b2c1b3ad2b73edc420c1112b1452b06deec4e0a
SuminPark <tnekf789@naver.com>
2018-10-30 02:31:56 +0000

    flask url 설정

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,6 +17,8 @@ def html_tag():
     return "<h1>안녕하세요</h1>"
     
 @app.route("/html_line")
+
+
 def html_line():
     return """
     <h1>여러 줄을 보내봅시다</h1>
@@ -24,3 +27,15 @@ def html_line():
         <li>2번</li>
     </ul>    
     """
+    
+    
+@app.route("/html_file")
+def html_fle():
+    return render_template("file.html")
+    
+    
+@app.route("/hello_p/<string:name>")
+def hello_p(name):
+    return render_template("hello.html", name = name)
+    
+    
